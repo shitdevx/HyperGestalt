9e4421721a04a9c2ff6d1d9a69acc40aa9bb769c
olaffx <olaffx@hypergestalt.local>
2026-08-29 12:56:19 +0200

    fix: fileport.h shim missing typedef fileport_t
    - Previous shim declared fileport_makeport using mach_port_t but code uses
      fileport_t type (src/main.m:183,191,522). Added typedef mach_port_t fileport_t
    - Update CI to create shim at external/.include (Makefile -I../.include) and sync
      from HyperGestalt/Bridging/include/sys/fileport.h

--- HyperGestalt/Bridging/include/sys/fileport.h
+++ HyperGestalt/Bridging/include/sys/fileport.h
@@ -1,10 +1,7 @@
 #ifndef _SYS_FILEPORT_H_
 #define _SYS_FILEPORT_H_
-#include <sys/cdefs.h>
-#include <sys/types.h>
 #include <mach/mach.h>
-__BEGIN_DECLS
+typedef mach_port_t fileport_t;
 kern_return_t fileport_makeport(int fd, mach_port_t *portname);
 int fileport_makefd(mach_port_t portname);
-__END_DECLS
 #endif
